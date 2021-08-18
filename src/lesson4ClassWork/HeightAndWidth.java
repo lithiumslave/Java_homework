aa01cba1c23cfb736caa14f4cf0f15777874e0b5
Malyarenko VItaliy <>
2021-08-18 23:17:34 +0300

    Refactored

--- src/lesson4ClassWork/HeightAndWidth.java
+++ src/lesson4ClassWork/HeightAndWidth.java
@@ -8,20 +8,20 @@ public class HeightAndWidth {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Please, enter height: ");
-        int m = scan.nextInt();
-        System.out.println("Please, enter width: ");
-        int n = scan.nextInt();
+        out.println("Please, enter height: ");
+        int height = scan.nextInt();
+        out.println("Please, enter width: ");
+        int width = scan.nextInt();
 
         // Loop in the loop
         out.println("Result: ");
-        for (int height = 0; height < m; height++) {
-            for (int width = 0; width < n; width++) {
-                System.out.printf("#");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                out.printf("#");
 
                 // Condition for the line break
-                if (width == (n - 1)) {
-                    System.out.println();
+                if (j == (width - 1)) {
+                    out.println();
                 }
             }
         }
